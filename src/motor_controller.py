20744ef2ea49e8e832710a7f94feb0b68fd9a19c
jpere180 <156122419+jpere180@users.noreply.github.com>
2024-02-13 08:30:08 -0800

    outline

--- src/motor_controller.py
+++ src/motor_controller.py
@@ -1,13 +1,13 @@
 """!
-@file encoder_reader.py
+@file motor_controller.py
 
-This program demonstrates the development of a class called Encoder to measure
-the movement of a motor using an optical encoder. This code was tested an ran
-with the Motor Driver class developed in Lab 1 to examine if the code was running
+This program demonstrates the development of a class called MotorController to that
+perform closed-loop proportional control. This code was tested an ranwith the Motor
+Driver class and Encoder clas developed in Lab 1 to examine if the code was running
 correctly. 
 
 @author Jessica Perez, Jacquelyn Banh, and Nathan Chapman
-@date   2024-02-06 Original program, based on example from above listed source
+@date   2024-02-13 Original program, based on example from above listed source
 @copyright (c) 2024 by Jessica Perez, Jacquelyn Banh, and Nathan Chapman and released under the GNU Public Licenes V3
 """
 
@@ -16,12 +16,12 @@ import time
 from motor_driver import MotorDriver
 from encoder_reader import Encoder
 
-class Encoder:
+class MotorController:
     """! 
-    This class implements a Encoder for an ME405 kit. 
+    This class implements the Motor Controller for an ME405 kit. 
     """
 
-    def __init__ (self, ch1pin, ch2pin, timer):
+    def __init__ (self, gain, setpoint, ch1pin, ch2pin, timer):
         """! 
         Creates an encoder object that can be used to measure
         the position of a motor
@@ -29,20 +29,29 @@ class Encoder:
         @param ch2pin: Pin for reading encoder channel 2
         @param timer: Timer object for reading encoder
         """
-        
-    def read(self):
+    
+    def run(self):
         """!
-        This method returns the current position of the
-        motor using the encoder
+        This method will repeatedly run the controll algorithm
+        """
+    
+    
+    def set_setpoint(self):
+        """!
+        This method sets up the setpoint for proportional control
         """
 
         
-    def zero(self):
+    def set_Kp(self):
         """!
-        This method sets the encoder count to zero at the
-        current position of the motor
+        This method sets up the gain for proportional control
         """
-        
+    
+    def controller_response(self):
+        """!
+        This method will print the results obtained of the step
+        response and print when the step response is done running
+        """  
 
 # This main code is run if this file is the main program but won't run if this
 # file is imported as a module by some other main program           
